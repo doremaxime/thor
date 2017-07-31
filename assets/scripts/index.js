1d65534a274c2783e952d31997ff93ee032602af
dore.maxime@gmail.com <dore.maxime@gmail.com>
2017-07-31 11:03:20 -0400

    Display web cam stream
    Upon page load, and with the user's permission, the user's web cam will
    be turned on and the player will stream the video to the canvas.
    
    Hid the player (top right webcam window).
    
    Added simple CSS to the photobooth class which houses the canvas to have
    the width be that of the window.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,9 +2,11 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin');
 const config = require('./config');
+const video = require('./video')
 
 $(() => {
   setAPIOrigin(location, config);
+  video;
 });
 
 // use require with a reference to bundle the file and use it in this file
