ffbf43e78a648b361961cf5eb78e54473fa97a14
dore.maxime@gmail.com <dore.maxime@gmail.com>
2017-07-31 12:26:50 -0400

    Voice feature - weather
    User can ask what is the weather in such or such city.
    ajax call will be made to the API openweathermap.com to fetch data.
    Speech synthesis will output the temperature of said city.
    
    snap.mp3 has been moved to assets/sound/

--- assets/scripts/voice.js
+++ assets/scripts/voice.js
@@ -5,6 +5,10 @@ const recognition = new SpeechRecognition();
 const voices = window.speechSynthesis.getVoices()
 const msg = new SpeechSynthesisUtterance();
 
+const lastWord = function(transcript) {
+  return (""+transcript).replace(/[\s-]+$/,'').split(/[\s-]/).pop();
+};
+
 function speak(e) {
   let transcript = Array.from(e.results)
     .map(result => result[0])
@@ -12,9 +16,22 @@ function speak(e) {
     .join('')
     console.log(transcript);
 
-  if(transcript.includes('test')) {
-    msg.text = ('echo');
+  if(transcript.includes('hey Thor')) {
+    msg.text = ('How may I help you?');
     speechSynthesis.speak(msg);
+  }
+
+  if(transcript.includes('what') && transcript.includes('weather') && transcript.includes('in')) {
+    let weather = 'http://api.openweathermap.org/data/2.5/weather?q=' + `${lastWord(transcript)}` + '&units=Imperial&appid=be4c79b49fa3a22cac7b890800d537b9';
+    console.log(weather);
+    $.ajax({
+      dataType: "jsonp",
+      url: weather,
+    }).then(function (results) {
+      console.log('results');
+      msg.text = ('It is ' + Math.round(results.main.temp) + 'degrees in ' + results.name)
+      speechSynthesis.speak(msg);
+    })
   }
 
 }
