adc5585dc12b02ac3c177c9f35896098575096a5
dore.maxime@gmail.com <dore.maxime@gmail.com>
2017-07-31 12:39:18 -0400

    Voice feature - Picture, Portfolio
    User can ask to take a picture, delete the last picture, and delete all
    the pictures.
    
    User can ask to open his Portfolio (currently hardcoded to be my
    portfolio).

--- assets/scripts/voice.js
+++ assets/scripts/voice.js
@@ -1,3 +1,5 @@
+const video = require('./video');
+
 //VOICE
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
@@ -6,7 +8,7 @@ const voices = window.speechSynthesis.getVoices()
 const msg = new SpeechSynthesisUtterance();
 
 const lastWord = function(transcript) {
-  return (""+transcript).replace(/[\s-]+$/,'').split(/[\s-]/).pop();
+  return ("" + transcript).replace(/[\s-]+$/, '').split(/[\s-]/).pop();
 };
 
 function speak(e) {
@@ -14,24 +16,41 @@ function speak(e) {
     .map(result => result[0])
     .map(result => result.transcript)
     .join('')
-    console.log(transcript);
+  console.log(transcript);
 
-  if(transcript.includes('hey Thor')) {
+  if (transcript.includes('hey Thor')) {
     msg.text = ('How may I help you?');
     speechSynthesis.speak(msg);
   }
 
-  if(transcript.includes('what') && transcript.includes('weather') && transcript.includes('in')) {
+  if (transcript.includes('what') && transcript.includes('weather') && transcript.includes('in')) {
     let weather = 'http://api.openweathermap.org/data/2.5/weather?q=' + `${lastWord(transcript)}` + '&units=Imperial&appid=be4c79b49fa3a22cac7b890800d537b9';
     console.log(weather);
     $.ajax({
       dataType: "jsonp",
       url: weather,
-    }).then(function (results) {
+    }).then(function(results) {
       console.log('results');
       msg.text = ('It is ' + Math.round(results.main.temp) + 'degrees in ' + results.name)
       speechSynthesis.speak(msg);
     })
+  }
+
+  if (transcript.includes('open') && transcript.includes('portfolio')) {
+    window.open('http://www.doremaxime.com', '_blank');
+  }
+  if (transcript.includes('close') && transcript.includes('portfolio')) {
+    window.close('http://www.doremaxime.com', '_blank');
+  }
+
+  if (transcript.includes('take') && transcript.includes('picture')) {
+    video.takePhoto();
+  }
+  if (transcript.includes('delete') && transcript.includes('all') && transcript.includes('pictures')) {
+    document.querySelector('.strip').remove();
+  }
+  if (transcript.includes('delete') && transcript.includes('last') && transcript.includes('picture')) {
+    document.querySelector('.strip').lastChild.remove();
   }
 
 }
