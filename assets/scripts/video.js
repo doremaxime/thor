15a2377d7521374ce1cdc980fa028f20028c308e
dore.maxime@gmail.com <dore.maxime@gmail.com>
2017-08-21 09:36:52 -0400

    Menu Development - all but game
    When user sets off the menu it will show 6 boxes (map, game, hire max,
    linkedin, github, and portfolio). a second time and it will hide them.
    
    Each box, except for game is operational.
    map shows the map
    hire max initiates an email to me
    linkedin, github, and portfolio open new tabs with the appropriate links
    
    Instructions in html have been updated.

--- assets/scripts/video.js
+++ assets/scripts/video.js
@@ -7,6 +7,9 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+let menu = document.querySelector('.menu-boxes');
+let webcam = document.querySelector('.player');
+let allButMap = document.querySelector('.all-but-map');
 let rgb = {
   r: 0,
   g: 0,
@@ -14,6 +17,12 @@ let rgb = {
 };
 let range;
 
+// Sets initial for the setIntervals
+menu.style.visibility = 'hidden';
+webcam.style.visibility = 'hidden';
+allButMap.style.visibility = 'hidden';
+
+// gets the video stream from the user's webcam
 function getVideo() {
   navigator.mediaDevices.getUserMedia({
       video: true,
@@ -27,6 +36,7 @@ function getVideo() {
     });
 }
 
+// Applies the webcam stream to a canvas
 function paintToCanvas() {
   const width = video.videoWidth;
   const height = video.videoHeight;
@@ -38,38 +48,126 @@ function paintToCanvas() {
   }, 16);
 }
 
-let pixelMatcher = setInterval(function() {
+let pixelMatcherOne = setInterval(function() {
   if (range !== undefined) {
-    bottomRightPixels();
     topRightPixels();
-    topLeftPixels();
   }
-}, 600);
+}, 1000);
 
 function bottomRightPixels() {
   pixelTemplate(0, 430, 50, 50);
   if ((rgb.r > range[0] && rgb.r < range[1]) && (rgb.g > range[2] && rgb.g < range[3]) && (rgb.b > range[4] && rgb.b < range[5])) {
-  console.log('bottom right');
+    // console.log('bottom right');
   }
 }
 
+// matches calibrated RGA with menu RGA
 function topRightPixels() {
-  pixelTemplate(0, 0, 50, 50);
+  pixelTemplate(0, 0, 50, 30);
   if ((rgb.r > range[0] && rgb.r < range[1]) && (rgb.g > range[2] && rgb.g < range[3]) && (rgb.b > range[4] && rgb.b < range[5])) {
-  console.log('top right');
-  window.open('https://www.cybba.com/', '_blank');
+    // console.log('top right');
+
+    // shows/hides the menu options
+    if (menu.style.visibility === 'hidden') {
+      menu.style.visibility = 'visible';
+    } else {
+      menu.style.visibility = 'hidden';
+    }
+    if (allButMap.style.visibility === 'visible') {
+      allButMap.style.visibility = 'hidden';
+    } else {
+      allButMap.style.visibility = 'visible';
+    }
+
   }
 }
 
-function topLeftPixels() {
-  pixelTemplate(590, 0, 50, 50);
+let pixelMatcherTwo = setInterval(function() {
+  if (menu.style.visibility === 'visible') {
+    topLeftOne();
+
+    // Does not get called if map is being shown.
+    if (allButMap.style.visibility === 'visible') {
+      topLeftTwo();
+      topLeftThree();
+      topLeftFour();
+      topLeftFive();
+      topLeftSix();
+    }
+  }
+}, 600);
+
+function topLeftOne() {
+  pixelTemplate(590, 0, 50, 30);
   if ((rgb.r > range[0] && rgb.r < range[1]) && (rgb.g > range[2] && rgb.g < range[3]) && (rgb.b > range[4] && rgb.b < range[5])) {
-  console.log('top left');
-  let container = document.querySelector('.flip-container');
-  container.classList.toggle('hover');
+    // console.log('Map');
+
+    // flips the large webcam view to show the map.
+    let container = document.querySelector('.flip-container');
+    container.classList.toggle('hover');
+
+    // Shows the top-right webcam view in when map is shown so as to know where to wave to hide the map.
+    if (webcam.style.visibility === 'hidden') {
+      webcam.style.visibility = 'visible';
+    } else {
+      webcam.style.visibility = 'hidden';
+    }
+
+    // makes sure other boxes cannot be set off as it is trickier to set off the map one on top-right webcam view.
+    if (allButMap.style.visibility === 'visible') {
+      allButMap.style.visibility = 'hidden';
+    } else {
+      allButMap.style.visibility = 'visible';
+    }
+
   }
 }
 
+function topLeftTwo() {
+  pixelTemplate(510, 0, 50, 30);
+  if ((rgb.r > range[0] && rgb.r < range[1]) && (rgb.g > range[2] && rgb.g < range[3]) && (rgb.b > range[4] && rgb.b < range[5])) {
+    // console.log('Game needs to be developed');
+    alert('Game is currently being developed, check back soon!')
+  }
+}
+
+function topLeftThree() {
+  pixelTemplate(430, 0, 50, 30);
+  if ((rgb.r > range[0] && rgb.r < range[1]) && (rgb.g > range[2] && rgb.g < range[3]) && (rgb.b > range[4] && rgb.b < range[5])) {
+    // console.log('hire max');
+    window.location.href = 'mailto:dore.maxime@gmail.com' + '?subject=Congratulations!&body=We would love to have you on our team!';
+    sleepFor(3000);
+  }
+}
+
+function topLeftFour() {
+  pixelTemplate(350, 0, 50, 30);
+  if ((rgb.r > range[0] && rgb.r < range[1]) && (rgb.g > range[2] && rgb.g < range[3]) && (rgb.b > range[4] && rgb.b < range[5])) {
+    // console.log('top left four');
+    window.open('https://www.linkedin.com/in/doremaxime/?locale=en_US', '_blank');
+    sleepFor(3000);
+  }
+}
+
+function topLeftFive() {
+  pixelTemplate(270, 0, 50, 30);
+  if ((rgb.r > range[0] && rgb.r < range[1]) && (rgb.g > range[2] && rgb.g < range[3]) && (rgb.b > range[4] && rgb.b < range[5])) {
+    // console.log('top left five');
+    window.open('https://github.com/doremaxime', '_blank');
+    sleepFor(3000);
+  }
+}
+
+function topLeftSix() {
+  pixelTemplate(190, 0, 50, 30);
+  if ((rgb.r > range[0] && rgb.r < range[1]) && (rgb.g > range[2] && rgb.g < range[3]) && (rgb.b > range[4] && rgb.b < range[5])) {
+    // console.log('top left six');
+    window.open('http://www.doremaxime.com', '_blank');
+    sleepFor(3000);
+  }
+}
+
+// Loops through the pixels in the requested area to get the average of the RGA
 let calibrate = function() {
   let rgb = {
     r: 0,
@@ -79,7 +177,8 @@ let calibrate = function() {
   let blockSize = 4;
   let i = -4;
   let count = 0;
-  let data = ctx.getImageData(0, 430, 50, 50);
+  let data = ctx.getImageData(0, 450, 30, 30);
+
   let length = data.data.length;
 
   while ((i += blockSize * 4) < length) {
@@ -100,10 +199,17 @@ let calibrate = function() {
   let blueLow = rgb.b - 40;
   let blueHigh = rgb.b + 40;
   range = [redLow, redHigh, greenLow, greenHigh, blueLow, blueHigh];
+
+  // helps see the area when selecting an area
+  // ctx.beginPath();
+  // ctx.rect(0, 0, 50, 50);
+  // ctx.fillStyle = 'yellow';
+  // ctx.fill();
 }
 
+// template get the average RGA in requested area to then be used for comparison
 function pixelTemplate(sx, sy, sw, sh) {
-  let blockSize = 4; // only visit every 5 pixels
+  let blockSize = 2; // only visit every 5 pixels
   let i = -4;
   let count = 0;
   let data = ctx.getImageData(sx, sy, sw, sh);
@@ -122,6 +228,7 @@ function pixelTemplate(sx, sy, sw, sh) {
   rgb.b = ~~(rgb.b / count);
 }
 
+// Takes a photo with the webcam and shows it (also downloadable)
 function takePhoto() {
   snap.currentTime = 0;
   snap.play();
@@ -133,6 +240,12 @@ function takePhoto() {
   link.textContent = ('Download Image');
   link.innerHTML = `<img src="${data}" alt="image" />`;
   strip.insertBefore(link, strip.firstChild);
+}
+
+// Used when opening tabs for practicality so as to not open multiple accidently
+function sleepFor(sleepDuration) {
+  var now = new Date().getTime();
+  while (new Date().getTime() < now + sleepDuration) { /* do nothing */ }
 }
 
 video.addEventListener('canplay', paintToCanvas);
